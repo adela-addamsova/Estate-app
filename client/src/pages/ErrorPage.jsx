289a53902adc06b5447fd8d5f992e1bffffafc78
adela-addamsova <a.addamsova@gmail.com>
2025-06-29 20:01:47 +0200

    fix: rename double used error-container class

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 import BackgroundLayout from '../components/BackgroundLayout';
 
@@ -5,7 +6,7 @@ import BackgroundLayout from '../components/BackgroundLayout';
 function ErrorPage() {
   return (
     <BackgroundLayout>
-      <div className="error-container">
+      <div className="error-page-container">
         <h3>Stránka neexistuje!</h3>
         <Link to="/chci-nabidku" className="form-button !w-25 !mx-auto !mt-4">
           Zpět
